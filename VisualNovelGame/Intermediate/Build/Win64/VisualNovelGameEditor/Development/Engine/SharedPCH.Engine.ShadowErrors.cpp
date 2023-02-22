168bdc0802f81ea8b0f6d21652d92fee62ca2f11
Ignat Mezhentsev <ignarmezh@gmail.com>
2023-02-22 11:11:07 +0400

    Initialize project (UE 5.0.3), git ignore.

---
+++ VisualNovelGame/Intermediate/Build/Win64/VisualNovelGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30139
+#include "D:/Personal/VisualNovelPrototype/VisualNovelGame/Intermediate/Build/Win64/VisualNovelGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
