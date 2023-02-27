7434a157382d30229a292eb34cd9bb5b27fa8e2f
Ignat Mezhentsev <ignarmezh@gmail.com>
2023-02-27 13:09:38 +0400

    Update gitignore

--- VisualNovelGame/Intermediate/Build/Win64/VisualNovelGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30139
-#include "D:/Personal/VisualNovelPrototype/VisualNovelGame/Intermediate/Build/Win64/VisualNovelGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
